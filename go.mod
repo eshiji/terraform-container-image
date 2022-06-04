8933126f56dfcbce069a76a8b1b564239966ab39
Rafael Eshiji Miazaki <eshijii@gmail.com>
2022-06-03 22:53:37 -0300

    Add terratest test

---
+++ go.mod
@@ -0,0 +1,13 @@
+module terraform-docker/m/v2
+
+go 1.18
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gruntwork-io/terratest v0.40.12 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ test/docker_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/docker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDocker(t *testing.T) {
+	// website::tag::1:: Configure the tag to use on the Docker image.
+	tag := "test"
+	buildOptions := &docker.BuildOptions{
+		Tags: []string{tag},
+	}
+
+	// website::tag::2:: Build the Docker image.
+	docker.Build(t, "../", buildOptions)
+
+	// website::tag::3:: Run the Docker image, read the text file from it, and make sure it contains the expected output.
+	go_version := &docker.RunOptions{Command: []string{"go", "version"}}
+	terragrunt_version := &docker.RunOptions{Command: []string{"terragrunt", "-v"}}
+
+	output_go := docker.Run(t, tag, go_version)
+	output_terragrunt := docker.Run(t, tag, terragrunt_version)
+	assert.Equal(t, "go version go1.18.2 linux/amd64", output_go)
+	assert.Equal(t, "terragrunt version v0.36.10", output_terragrunt)
+}
